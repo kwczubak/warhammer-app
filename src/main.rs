002c1490845c30a82bfb1ea44ec0f2624c7912f6
Kyle Czubak <kyleczubak@gmail.com>
2022-07-25 23:13:14 -0500

    Think I finished all of the structs needed for the ros file. Can't figure out how to load from file.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use std::{fs::File, io::BufReader};
+
+use serde_xml_rs::{from_str, from_reader};
+
+mod ros_parser;
+
 fn main() {
-    println!("Hello, world!");
+
+    let file = File::open("Test_Roster.ros")?;
+    // let buf = BufReader::new(f);
+
+    // let doc : ros_parser::Selections = from_reader(buf).unwrap();
+
+    // println!("{:#?}", doc);
 }
