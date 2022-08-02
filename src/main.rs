8f559f565d74b21b60d17405807d9b81b45c3b1b
Kyle Czubak <kyleczubak@gmail.com>
2022-08-01 19:19:28 -0500

    I hate javascript. Moving to an gui app in rust.

--- src/main.rs
+++ src/main.rs
@@ -6,78 +6,35 @@ use crate::warhammer_data::ros_parser::ros_parser::Roster;
 
 mod warhammer_data;
 
-fn main() -> std::io::Result<()> {
-    let file = File::open("Astra_1000/Astra_1000.ros")?;
-    let buf = BufReader::new(file);
+use druid::widget::{Button, Flex, Label};
+use druid::{AppLauncher, LocalizedString, PlatformError, Widget, WidgetExt, WindowDesc};
 
-    let roster: Roster = from_reader(buf).unwrap();
-    // println!("{:#?}", roster);
+fn ui_builder() -> impl Widget<u32> {
+    // The label text will be computed dynamically based on the current locale and count
+    let text =
+        LocalizedString::new("hello-counter").with_arg("count", |data: &u32, _env| (*data).into());
+    let label = Label::new(text).padding(5.0).center();
+    let button = Button::new("increment")
+        .on_click(|_ctx, data, _env| *data += 1)
+        .padding(5.0);
 
-    // let profile = warhammer_data::unit::ModelProfile::from_profile(
-    //     &roster.forces.forces[0].selections.selections[4]
-    //         .profiles
-    //         .as_ref()
-    //         .unwrap()
-    //         .profiles[0]
-    // );
-    // println!("{:#?}", profile);
+    Flex::column().with_child(label).with_child(button)
+}
 
-    // let profile = warhammer_data::weapon::parse_weapon_profile(
-    //     &roster.forces.forces[0].selections.selections[4]
-    //         .selections
-    //         .as_ref()
-    //         .unwrap()
-    //         .selections[0]
-    //         .profiles
-    //         .as_ref()
-    //         .unwrap()
-    //         .profiles[0]
-    //         .characteristics,
-    // );
-    // println!("{:#?}", profile);
+fn main() -> Result<(), PlatformError> {
+    // let file = File::open("Astra_1000/Astra_1000.ros")?;
+    // let buf = BufReader::new(file);
 
-    // let model = warhammer_data::unit::parse_model(
+    // let roster: Roster = from_reader(buf).unwrap();
+    // let army = warhammer_data::army::Army::from_roster(
     //     &roster
-    //         .forces
-    //         .forces
-    //         .get(0)
-    //         .unwrap()
-    //         .selections
-    //         .selections
-    //         .get(4)
-    //         .unwrap(),
     // );
-    // println!("{:#?}", model);
 
-    // let unit = warhammer_data::unit::Unit::from_selection(
-    //     &roster
-    //         .forces
-    //         .forces
-    //         .get(0)
-    //         .unwrap()
-    //         .selections
-    //         .selections
-    //         .get(11)
-    //         .unwrap(),
-    // );
-    // println!("{:#?}", unit);
-
-    // let detachment = warhammer_data::army::Detachment::from_force(
-    //     &roster
-    //         .forces
-    //         .forces
-    //         .get(0)
-    //         .unwrap()
-    // );
-    // println!("{:#?}", detachment);
-
-    let army = warhammer_data::army::Army::from_roster(
-        &roster
-    );
-    println!("{:#?}", army);
-
-    // let d = warhammer_data::profile::ProfileValue::from_str("2D3+1");
-    // print!("{:#?}", d);
+    let main_window = WindowDesc::new(ui_builder);
+    let data = 0_u32;
+    AppLauncher::with_window(main_window)
+        .use_simple_logger()
+        .launch(data)?;
 
     Ok(())
 }
